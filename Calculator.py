b4094b13755f425903412514b424b501a9c90bf3
Geethu37-TOP <ggeethu492@gmail.com>
2025-08-29 15:08:16 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))  # e.g. 5
+num2 = int(input("Enter second number: ")) # e.g. 3
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} % {num2} = {num1 % num2}")
